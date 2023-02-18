ff3878f2fede66ba77510d15ce8dc126878ac293
tomoron <tomoron326@outlook.jp>
2023-02-18 15:59:33 +0900

    setup:tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
+        <p className="text-red-600">
+          Get started by Next.js
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
@@ -18,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -87,5 +87,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/app/*.{js,jsx,ts,tsx}", "./src/pages/*.{js,jsx,ts,tsx]"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

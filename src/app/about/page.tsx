5c2a7aa5f971fa3cc1c1705b1ccb444fc289acbd
tomoron <tomoron326@outlook.jp>
2023-02-18 21:04:59 +0900

    add:rippleui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
 export default function About() {
   return (
     <main>
+      <div className='navbar'>
+        <div className='navbar-start'>
+          <a href='/' className='navbar-item'>
+            Title
+          </a>
+          <button className='btn btn-primary'>Default</button>
+        </div>
+      </div>
       <div>
-        <h1 className="text-red-500">About</h1>
+        <h1 className='text-red-500'>About</h1>
       </div>
     </main>
-  );
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require('rippleui')],
+}

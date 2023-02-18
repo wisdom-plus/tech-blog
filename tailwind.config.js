508808f56bca072021e42fd6a5aba3639746bab2
tomoron <tomoron326@outlook.jp>
2023-02-18 20:03:53 +0900

    eslint & prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 
 module.exports = {
   content: [
-    "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/app/*.{js,jsx,tx,tsx}",
-    "./src/pages/*.{js,jsx,ts,tsx]",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

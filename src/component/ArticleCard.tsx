0a51d57c7f4c26c717138d78bec5fd9c364d0240
tomoron <tomoron326@outlook.jp>
2023-02-25 18:45:23 +0900

    add:systly

--- src/component/ArticleCard.tsx
+++ src/component/ArticleCard.tsx
@@ -1,6 +1,6 @@
 const ArticleCard = () => {
   return (
-    <div className='card'>
+    <div className='card w-1/3 m-4'>
       <div className='card-body'>
         <h2 className='card-header'>タイトル</h2>
         <p className='card-content'>ブログの内容の文章が入ります</p>

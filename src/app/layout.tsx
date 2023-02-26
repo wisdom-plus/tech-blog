2694480649a47bd57e3e50260fad0e1286dc484b
tomoron <tomoron326@outlook.jp>
2023-02-26 20:02:33 +0900

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,29 @@
+import { Merriweather, Merriweather_Sans } from '@next/font/google'
 import './globals.css'
+
+const merriweather = Merriweather({
+  variable: '--font-merriweather',
+  display: 'swap',
+  weight: '300',
+  subsets: ['latin'],
+})
+
+const merriweather_sans = Merriweather_Sans({
+  variable: '--font-merriweather-sans',
+  display: 'swap',
+  weight: '300',
+  subsets: ['latin'],
+})
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang='jp'>
+    <html lang='jp' className={`${merriweather.variable} ${merriweather_sans.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className='font-sans leading-normal tracking-normal'>{children}</body>
+      <body className='leading-normal tracking-normal'>{children}</body>
     </html>
   )
 }
--- src/component/HeaderNav.tsx
+++ src/component/HeaderNav.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image'
+
 const HeaderNav = () => {
   return (
     <div className='navbar h-20'>
       <div className='navbar-start'>
-        <a className='navbar-item text-xl' href='/'>
-          Tech-blog
+        <a href='/' className='navbar-item flex'>
+          <Image src='/logo-white.png' width={50} height={50} alt='logo' />
+          <div className='text-2xl mt-2 font-merriweather'>TechAmply</div>
         </a>
-        <a className='navbar-item mr-4' href='/'>
+        <a className='navbar-item' href='/'>
           ブログ
         </a>
       </div>

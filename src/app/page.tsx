f76edb7fcb4ca46ca0c3bf56d5d41b3d89dcb74a
tomoron <tomoron326@outlook.jp>
2023-03-10 22:22:46 +0900

    top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,17 @@ import ArticleCardRow from '@/component/ArticleCardRow'
 const Root = () => {
   return (
     <main className='container h-full w-full mx-auto my-20 top-16'>
-      <div className='flex flex-row justify-center items-center h-full w-full mx-24'>
-        <div className='max-w-4xl flex flex-col h-full w-full'>
+      <div className='flex flex-row justify-center items-center h-full w-full mx-auto max-w-7xl'>
+        <div className='flex flex-col h-full w-full'>
           <h1 className='text-4xl'>最新の投稿</h1>
           <div className='divider'></div>
-          <div className='max-w-7xl h-full w-full flex flex-wrap justify-evenly'>
+          <div className='max-w-7xl h-full w-full flex flex-wrap justify-evenly space-x-3'>
             <ArticleCard />
             <ArticleCard />
             <ArticleCard />
             <ArticleCard />
             <ArticleCard />
             <ArticleCard />
-          </div>
-        </div>
-        <div className='container max-w-sm'>
-          <h2 className='text-2xl'>Ranking</h2>
-          <div className='divider'></div>
-          <div className='container'>
-            <ArticleCardRow />
-            <ArticleCardRow />
-            <ArticleCardRow />
-            <ArticleCardRow />
           </div>
         </div>
       </div>

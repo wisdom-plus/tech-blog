f24b2daa8df4f15c3e3e9bf2ce06336c7d5aae86
tomoron <tomoron326@outlook.jp>
2023-03-11 21:53:06 +0900

    fix:padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ const Root = () => {
       <main className='container h-full w-full mx-auto my-20 top-16'>
         <div className='flex flex-row justify-center items-center h-full w-full mx-auto max-w-7xl'>
           <div className='flex flex-col h-full w-full'>
-            <h1 className='text-4xl'>最新の投稿</h1>
-            <div className='divider'></div>
             <div className='max-w-7xl h-full w-full flex flex-wrap justify-evenly space-x-3'>
               <ArticleCard />
               <ArticleCard />
--- src/component/BackImage.tsx
+++ src/component/BackImage.tsx
@@ -9,7 +9,7 @@ const BackImage = ({ title }: Props) => {
     <div className='top-image bg-center bg-cover min-h-210 mt-2'>
       <div className='max-w-7xl w-full h-full justify-center items-center mx-auto'>
         <Breadcrumb />
-        <h1 className='text-4xl'>{title}</h1>
+        <h1 className='text-5xl pt-12 px-4'>{title}</h1>
       </div>
     </div>
   )

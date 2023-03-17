ed99385d1e146c3a62f7a0c3dc50dd4462c7a855
tomoron <tomoron326@outlook.jp>
2023-03-17 23:24:09 +0900

    add:step

--- src/app/administrator/page.tsx
+++ src/app/administrator/page.tsx
@@ -28,11 +28,110 @@ const Page = () => {
             大阪府枚方市
           </p>
           <h3 className='text-xl font-bold pt-6'>経歴</h3>
-          <p className='pt-2 text-sm'>
-            1997年3月26日に大阪に生まれる。数度の転校をし大阪府枚方市に移住
-          </p>
-          <p className='pt-2 text-sm'>2015年3月に長尾高校を卒業。公務員を目指しフリーターになる</p>
-          <p className='pt-2 text-sm'>2年試験に落ち公務員になるのを諦める</p>
+          <ol className='steps steps-vertical items-start pt-6'>
+            <div className='flex flex-row'>
+              <li className='step'>
+                <div className='step-circle'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z'
+                    />
+                  </svg>
+                </div>
+                <p className='text-sm'>1996年 3月</p>
+              </li>
+              <p className='pt-2 pl-3 text-sm'>大阪に生まれる。数度の転校をし大阪府枚方市に移住</p>
+            </div>
+            <div className='flex flex-row'>
+              <li className='step'>
+                <div className='step-circle'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5'
+                    />
+                  </svg>
+                </div>
+                <p className='text-sm'>
+                  2015年 3月
+                  <br />
+                  　　~　　
+                  <br />
+                  2017年
+                </p>
+              </li>
+              <p className='pt-2 pl-3 text-sm'>
+                長尾高校を卒業。公務員を目指しフリーターになる
+                <br />
+                2年試験に落ち公務員になるのを諦める
+              </p>
+            </div>
+            <div className='flex flex-row'>
+              <li className='step'>
+                <div className='step-circle'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21'
+                    />
+                  </svg>
+                </div>
+                <p className='text-sm'>2020年 4月</p>
+              </li>
+              <p className='pt-2 pl-3 text-sm'>
+                その後4年ほどフリーターを続け、コロナでのロックダウン時にプログラミングを始める
+                <br />
+                プログラミングの面白さと奥深さに魅了され、エンジニアを志す
+              </p>
+            </div>
+            <div className='flex flex-row'>
+              <li className='step-end flex flex-col items-center gap-2 w-fit'>
+                <div className='step-circle'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke-width='1.5'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25'
+                    />
+                  </svg>
+                </div>
+                <p className='text-sm'>2023年 1月</p>
+              </li>
+              <p className='pt-2 pl-3 text-sm'>
+                フルリモート受託開発に採用
+                <br />
+                開発に関わりながら成長中
+              </p>
+            </div>
+          </ol>
 
           <div className='mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center'>
             <a className='link mr-6' href='#' data-tippy-content='@twitter_handle'>

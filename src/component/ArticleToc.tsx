379950d5506bbd5720a1a394d479e5947aa8c92d
tomoron <tomoron326@outlook.jp>
2023-03-23 22:02:50 +0900

    toc

---
+++ src/component/ArticleToc.tsx
@@ -0,0 +1,42 @@
+import { load } from 'cheerio'
+
+const typography = (tag: string) => {
+  if (tag === 'h1') {
+    return ''
+  } else if (tag === 'h2') {
+    return 'pl-4'
+  } else if (tag === 'h3') {
+    return 'pl-8'
+  }
+}
+
+const ArticleToc = ({ body }: { body: string }) => {
+  const $ = load(body)
+  const headings = $('h1, h2, h3').toArray()
+  const toc = headings.map((data) => ({
+    text: $(data).text(),
+    id: $(data).attr('id'),
+    rank: $(data).prop('tagName').toLowerCase(),
+  }))
+
+  return (
+    <div className='flex flex-col w-full mx-auto rounded-xl bg-gray-3 max-w-sm'>
+      <div className='ml-4 p-3 article-toc'>
+        <div className='text-left mb-3 text-xl'>目次</div>
+        <div className='flex flex-col'>
+          {toc.map((data) => (
+            <a
+              href={`#${data.id}`}
+              key={data.id}
+              className={`text-left mb-2 p-1 hover:bg-gray-5` + ` ${typography(data.rank)}`}
+            >
+              <div className=''>{data.text}</div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ArticleToc

d4051c9142ea2f0be4c78de9b779994dbe899099
tomoron <tomoron326@outlook.jp>
2023-04-07 21:30:30 +0900

    icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,7 +26,7 @@ const Page = () => {
                 どうか長い目で応援していただけるとうれしいです。よかったら
                 <Link
                   className='link link-ghost link-underline'
-                  href='https://twitter.com/wisdom_puls_jp'
+                  href='https://twitter.com/wisdom_plus_jp'
                 >
                   著者のツイッター
                 </Link>

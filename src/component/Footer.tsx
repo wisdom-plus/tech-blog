c07e7210a9e0c372ea1de881056f526e1a7c14ff
tomoron <tomoron326@outlook.jp>
2023-04-19 20:12:16 +0900

    add:rss icon

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -78,6 +78,24 @@ const Footer = () => {
                 </svg>
               </Link>
             </li>
+            <li>
+              <Link href='https://techamply.com/feed'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  width='32'
+                  height='32'
+                  strokeWidth={1.5}
+                  className='fill-gray-300'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+                  />
+                </svg>
+              </Link>
+            </li>
           </ul>
         </div>
       </div>

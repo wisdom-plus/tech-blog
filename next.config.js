32865ebc01e4eb952d969736b7ebd1b9e8aef5a2
wisdom-plus <tomoron326@outlook.jp>
2024-01-21 08:44:54 +0900

    update:next.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     dirs: ['app', 'pages', 'utils', 'components', 'hooks', 'types', 'public', 'api'],
   },

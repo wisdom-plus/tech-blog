5cde77a5fe4132537506e6e161629dfe0500747e
wisdom-plus <tomoron326@outlook.jp>
2024-12-09 09:08:39 +0900

    upgrade:next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     dirs: ['app', 'pages', 'utils', 'components', 'hooks', 'types', 'public', 'api'],
   },

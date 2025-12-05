a96370ecc3c07cb35064e36780f002bacdd6f56c
wisdom-plus <tomoron326@outlook.jp>
2025-12-05 15:51:01 +0900

    add:biome confing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,8 @@
-import nextTypescript from "eslint-config-next/typescript";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import _import from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs (一時的なダミー)
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...nextTypescript, ...fixupConfigRules(compat.extends(
-    "next/core-web-vitals",
-    "plugin:import/typescript",
-    "plugin:import/recommended",
-    "prettier",
-    "plugin:storybook/recommended",
-)), {
-    plugins: {
-        import: fixupPluginRules(_import),
-        "unused-imports": unusedImports,
-    },
-
-    rules: {
-        "import/order": ["error", {
-            alphabetize: {
-                order: "asc",
-            },
-        }],
-
-        "@next/next/no-html-link-for-pages": ["error", "src/app"],
-    },
-}, {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}];
+export default [
+  {
+    ignores: ['node_modules/**'],
+    rules: {}, // 何もルールを入れない
+  },
+]

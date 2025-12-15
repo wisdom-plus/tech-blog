28d764059a073ee124c8fb9b9a954c44e79ed831
wisdom-plus <tomoron326@outlook.jp>
2025-12-15 09:14:00 +0900

    fix:build error

--- src/component/TagArea.tsx
+++ src/component/TagArea.tsx
@@ -1,22 +1,22 @@
-"use client";
-import Link from "next/link";
-import type { tag } from "../types";
+'use client'
+import Link from 'next/link'
+import type { Tag } from '../types'
 
-const TagArea = ({ tags }: { tags: tag[] }) => {
+const TagArea = ({ tags }: { tags: Tag[] }) => {
   return (
-    <div className="badge-area">
+    <div className='badge-area'>
       {tags.map((tag) => (
         <Link
           href={`/tag/${tag.id}`}
           key={tag.id}
-          className="badge badge-lg text-sm badge-outline-primary mr-2 px-4 py-1"
+          className='badge badge-lg text-sm badge-outline-primary mr-2 px-4 py-1'
           style={{ color: tag.color, borderColor: tag.color }}
         >
           {tag.title}
         </Link>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default TagArea;
+export default TagArea

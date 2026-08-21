d8b49037a76889895e1695c3ef9f1d5104aa55f6
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-08-22 08:02:33 +0900

    pref:gtag type

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -2,9 +2,11 @@ export const GA_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
 
 export const pageview = (url: string) => {
   if (!GA_ID) return;
-  window.gtag("config", GA_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined"){
+    window.gtag("config", GA_ID, {
+      page_path: url,
+    });
+  }
 };
 
 export const event = ({
@@ -19,9 +21,11 @@ export const event = ({
   value?: number;
 }): void => {
   if (!GA_ID) return;
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (typeof window !== "undefined") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };

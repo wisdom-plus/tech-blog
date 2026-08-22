e601c676720670e8fbe91e5a866f7e030f2c4f71
wisdom-plus <wisdom-plus@users.noreply.github.com>
2026-08-23 08:02:33 +0900

    fix:gtag types

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,8 +1,14 @@
 export const GA_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
 
+declare global {
+  interface Window {
+    gtag: Gtag.Gtag;
+  }
+}
+
 export const pageview = (url: string) => {
   if (!GA_ID) return;
-  if (typeof window !== "undefined"){
+  if (typeof window !== "undefined") {
     window.gtag("config", GA_ID, {
       page_path: url,
     });
